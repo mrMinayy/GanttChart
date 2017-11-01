69793bfa037a3e23a500b565c5fc8bf9724811dc
AshkanMinayy <ashkanminayy@emadhejazians-macbook-pro.local>
2017-11-01 22:16:56 +0330

    Initial Commit

---
+++ Gantt Chart/Gantt_Chart.h
@@ -0,0 +1,19 @@
+//
+//  Gantt_Chart.h
+//  Gantt Chart
+//
+//  Created by AshkanMinayy on 11/1/17.
+//  Copyright © 2017 AshkanMinayy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gantt_Chart.
+FOUNDATION_EXPORT double Gantt_ChartVersionNumber;
+
+//! Project version string for Gantt_Chart.
+FOUNDATION_EXPORT const unsigned char Gantt_ChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gantt_Chart/PublicHeader.h>
+
+
